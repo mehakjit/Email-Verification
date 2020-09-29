ffe27e080f984282df1846fda5259dc82dc1a2f7
mehakjit <mehakjitsingh@gmail.com>
2020-09-29 07:48:27 +0530

    [Mehakjit] Added: Manadatory .co Validation

--- src/com/capg/EmailVerify.java
+++ src/com/capg/EmailVerify.java
@@ -1,7 +1,22 @@
 package com.capg;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EmailVerify {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Email validatation Program");
+			Scanner sc = new Scanner(System.in);
+			Pattern pincode = Pattern.compile("^abc{1,}[a-zA-Z0-9!@#$&()\\\\-`.+,/\\\"]*@bridgelab.co");
+			System.out.println("Enter the email");
+			String pattern = sc.nextLine();
+			Matcher matcher = pincode.matcher(pattern);
+			boolean isFound = matcher.find();
+			if(isFound)
+				System.out.println("Patter found");
+			else
+				System.out.println("Patter not found");
+			sc.close();
+		}
 	}
-}
